aff6689f70d21788dcd492e396582ab6016dc871
Achintya777 <59294509+Achintya777@users.noreply.github.com>
2020-09-15 16:40:20 +0530

    Create Sparse_table.cpp

---
+++ Sparse_table.cpp
@@ -0,0 +1,25 @@
+//rmq 
+#include <bits/stdc++.h>
+using namespace std ;
+const int mxN =1e5+1;
+int n,a[mxN],st[mxN][18] ;
+signed main(){
+  cin >>n ;
+  for(int i=0;i<n;i++)
+    cin >> a[i] ;
+  for(int i=0;i<n;i++)
+    st[i][0]=a[i] ;
+  for(int j=1;j<18;j++)
+    for(int i=0;i+(1<<j)<=n;i++)
+      st[i][j]=__gcd(st[i][j-1],st[i+(1<<j-1)][j-1]) ;
+  int q ;cin >> q ;
+  while(q--){
+    int x;cin >> x ;
+    
+    int ans=1e9 ;
+    for(int i=17;~i;--i)
+      if(l+(1<<i)<=r+1)
+        ans=min(ans,st[l][i]),l+=(1<<i) ;
+    cout << ans << '\n'  ;
+  }
+}
